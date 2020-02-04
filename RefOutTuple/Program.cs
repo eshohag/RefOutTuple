0722f30682d852f455ab684d0ab85780d3f3314a
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2020-02-04 11:54:23 +0600

    Add project files.

---
+++ RefOutTuple/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace RefOutTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region refKeyword
+            string refMessage = "";
+            string refMethod = GetRefTest(ref refMessage);
+            Console.WriteLine(refMessage);
+            Console.WriteLine(refMethod);
+
+            #endregion
+
+            #region outKeyword
+            string outMethod = GetOutTest(out string outMessage);
+            Console.WriteLine(outMessage);
+            Console.WriteLine(outMethod);
+
+            #endregion
+
+            #region Tuple
+            var tupleMethod = GetTupleTest("Richard");
+            Console.WriteLine(tupleMethod.Hi);
+            Console.WriteLine(tupleMethod.Message);
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        private static (string Hi, string Message) GetTupleTest(string customer)
+        {
+            return ("Hi-" + customer, "Here is tuple method returns");
+        }
+
+        private static string GetOutTest(out string message)
+        {
+            message = "Message- Calling out inside methods";
+            return "Calling Out Methods";
+        }
+
+        static string GetRefTest(ref string message)
+        {
+            message = "Message- Calling ref inside methods";
+            return "Calling Ref Methods";
+        }
+    }
+}
